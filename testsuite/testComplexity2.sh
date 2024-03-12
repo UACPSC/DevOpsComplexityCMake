5dc3101a960b4fc1ee3bd0bae5e15da60045ed21
Michael L. Collard <collard@uakron.edu>
2024-03-12 15:54:51 -0400

    Fix test case

--- testsuite/testComplexity2.sh
+++ testsuite/testComplexity2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srcml --text="void f(){ if (a < b); }" -l C++ | ./srccomplexity | grep -Fx "1" > /dev/null
+srcml --text="void f(){ if (a < b); }" -l C++ | ./srccomplexity | grep -Fx "2" > /dev/null
